dd8161d5cc50f2a0b7df8e42e057db2c8b3ee73f
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2023-12-29 14:17:47 +0000

    Created VS project from ASP.net template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Coursework2001.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
